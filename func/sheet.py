73354b5305fd340dff7f648181c31180f7eacc29
DKarneckij <dkarneckij@gmail.com>
2023-01-22 10:55:36 -0500

    >900 price

--- func/sheet.py
+++ func/sheet.py
@@ -63,7 +63,10 @@ class sheet():
 
             if soup_tag != 'None': 
                 regex = re.search('"text-price-large">\$(.+?) <span', soup_tag)
-                price = float(regex.group(1))
+                if len(regex.group(1)) > 6:
+                    prince = float(regex.group(1).replace(",", ""))
+                else:
+                    price = float(regex.group(1))
                 isbn = helper.get_isbn(soup)
                 return [price, isbn]
 
